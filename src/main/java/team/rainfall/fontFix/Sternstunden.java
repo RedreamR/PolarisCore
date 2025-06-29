64421a54a78c05fc06387b9b32b7bd225fd9c956
RedreamR <crystall1ne@outlook.com>
2025-06-29 11:04:53 +0800

    [refactor] ignore Sternstunden files

--- src/main/java/team/rainfall/fontFix/Sternstunden.java
+++
@@ -1,7 +0,0 @@
-package team.rainfall.fontFix;
-
-@SuppressWarnings("unused")
-public class Sternstunden {
-    public static native String getCopyrightString();
-    public static native String getVersion();
-}
