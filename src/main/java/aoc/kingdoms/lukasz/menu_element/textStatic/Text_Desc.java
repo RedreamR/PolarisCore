0f83a76b751edec45ae4127d95bec205d8f19bf9
RedreamR <crystall1ne@outlook.com>
2025-07-01 08:34:35 +0800

    [add] an api for disabling background render in Text_Desc

--- src/main/java/aoc/kingdoms/lukasz/menu_element/textStatic/Text_Desc.java
+++ src/main/java/aoc/kingdoms/lukasz/menu_element/textStatic/Text_Desc.java
@@ -26,7 +26,7 @@ import java.util.List;
 
 public class Text_Desc extends Text_Static {
     public ArrayList<Line> ffLines = new ArrayList<>();
-
+    public boolean extraRender = true;
 
     public Text_Desc(String sText, int iPosX, int iPosY, int iWidth) {
         this.init(sText, iPosX, iPosY, iWidth, CFG.FONT_REGULAR_SMALL);
@@ -57,14 +57,16 @@ public class Text_Desc extends Text_Static {
     }
 
     public void draw(SpriteBatch oSB, int iTranslateX, int iTranslateY, boolean isActive, boolean scrollableY) {
-        oSB.setColor(new Color(Colors.COLOR_STATS_RECT_BG.r, Colors.COLOR_STATS_RECT_BG.g, Colors.COLOR_STATS_RECT_BG.b, getBoxAlpha(this.getClickable(), this.getIsHovered(), isActive)));
-        Renderer.drawBox(oSB, Images.statsRectBG, this.getPosX() + iTranslateX, this.getPosY() + iTranslateY, this.getWidth(), this.getHeight(), 0.8F);
-        oSB.setColor(new Color(Colors.COLOR_GRADIENT.r, Colors.COLOR_GRADIENT.g, Colors.COLOR_GRADIENT.b, 0.175F));
-        Images.gradientFull.draw(oSB, this.getPosX() + iTranslateX, this.getPosY() + iTranslateY, this.getWidth(), this.getHeight());
-        oSB.setColor(new Color(Colors.COLOR_GRADIENT.r, Colors.COLOR_GRADIENT.g, Colors.COLOR_GRADIENT.b, 0.3F));
-        Images.gradientXY.draw(oSB, this.getPosX() + iTranslateX, this.getPosY() + iTranslateY, this.getWidth(), this.getHeight(), false, true);
-        oSB.setColor(new Color(0.0F, 0.0F, 0.0F, 0.45F));
-        Renderer.drawBox(oSB, Images.statsRectBGBorder, this.getPosX() + iTranslateX, this.getPosY() + iTranslateY, this.getWidth(), this.getHeight(), 1.0F);
+        if(extraRender) {
+            oSB.setColor(new Color(Colors.COLOR_STATS_RECT_BG.r, Colors.COLOR_STATS_RECT_BG.g, Colors.COLOR_STATS_RECT_BG.b, getBoxAlpha(this.getClickable(), this.getIsHovered(), isActive)));
+            Renderer.drawBox(oSB, Images.statsRectBG, this.getPosX() + iTranslateX, this.getPosY() + iTranslateY, this.getWidth(), this.getHeight(), 0.8F);
+            oSB.setColor(new Color(Colors.COLOR_GRADIENT.r, Colors.COLOR_GRADIENT.g, Colors.COLOR_GRADIENT.b, 0.175F));
+            Images.gradientFull.draw(oSB, this.getPosX() + iTranslateX, this.getPosY() + iTranslateY, this.getWidth(), this.getHeight());
+            oSB.setColor(new Color(Colors.COLOR_GRADIENT.r, Colors.COLOR_GRADIENT.g, Colors.COLOR_GRADIENT.b, 0.3F));
+            Images.gradientXY.draw(oSB, this.getPosX() + iTranslateX, this.getPosY() + iTranslateY, this.getWidth(), this.getHeight(), false, true);
+            oSB.setColor(new Color(0.0F, 0.0F, 0.0F, 0.45F));
+            Renderer.drawBox(oSB, Images.statsRectBGBorder, this.getPosX() + iTranslateX, this.getPosY() + iTranslateY, this.getWidth(), this.getHeight(), 1.0F);
+        }
         oSB.setColor(Color.WHITE);
         for(int i = 0; i < this.ffLines.size(); ++i) {
             TextColorRenderer.drawLine(oSB, this.fontID, this.ffLines.get(i), this.getPosX() + this.getPadding() + iTranslateX, this.getPosY() + this.getPaddingY() + (this.iTextHeight + CFG.PADDING * 2) * i + iTranslateY, this.getColor(isActive));
