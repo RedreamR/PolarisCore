12f0534006cd596233cb905252f845473e6a3fd9
RedreamR <crystall1ne@outlook.com>
2025-07-22 19:20:10 +0800

    [refactor] remove unnecessary type converting

--- src/main/java/team/rainfall/fontFix/MixinMap.java
+++ src/main/java/team/rainfall/fontFix/MixinMap.java
@@ -18,13 +18,13 @@ public class MixinMap {
             this.iActiveMapID = nActiveMapID;
             this.updateWorldMap();
         }
-        Game.DRAW_CITIES_MIN_SCALE = ((Map_Data)this.lMaps.get(this.iActiveMapID)).mapData.DRAW_CITIES_MIN_SCALE;
-        Game.DRAW_CIV_NAMES_START_DRAWING_MAP_SCALE = ((Map_Data)this.lMaps.get(this.iActiveMapID)).mapData.DRAW_CIV_NAMES_START_DRAWING_MAP_SCALE;
-        Game.DRAW_INNER_BORDERS = ((Map_Data)this.lMaps.get(this.iActiveMapID)).mapData.DRAW_INNER_BORDERS;
-        Game.DRAW_OCCUPIED_PROVINCES_MIN_SCALE = ((Map_Data)this.lMaps.get(this.iActiveMapID)).mapData.DRAW_OCCUPIED_PROVINCES_MIN_SCALE;
-        Game.DRAW_ARMY_MIN_SCALE = ((Map_Data)this.lMaps.get(this.iActiveMapID)).mapData.DRAW_ARMY_MIN_SCALE;
-        Game.DRAW_OCCUPIED_SCALE = ((Map_Data)this.lMaps.get(this.iActiveMapID)).mapData.DRAW_OCCUPIED_SCALE;
-        TechnologyTree.MapResearchCost = ((Map_Data)this.lMaps.get(this.iActiveMapID)).mapData.ResearchCost;
+        Game.DRAW_CITIES_MIN_SCALE = this.lMaps.get(this.iActiveMapID).mapData.DRAW_CITIES_MIN_SCALE;
+        Game.DRAW_CIV_NAMES_START_DRAWING_MAP_SCALE = this.lMaps.get(this.iActiveMapID).mapData.DRAW_CIV_NAMES_START_DRAWING_MAP_SCALE;
+        Game.DRAW_INNER_BORDERS = this.lMaps.get(this.iActiveMapID).mapData.DRAW_INNER_BORDERS;
+        Game.DRAW_OCCUPIED_PROVINCES_MIN_SCALE = this.lMaps.get(this.iActiveMapID).mapData.DRAW_OCCUPIED_PROVINCES_MIN_SCALE;
+        Game.DRAW_ARMY_MIN_SCALE = this.lMaps.get(this.iActiveMapID).mapData.DRAW_ARMY_MIN_SCALE;
+        Game.DRAW_OCCUPIED_SCALE = this.lMaps.get(this.iActiveMapID).mapData.DRAW_OCCUPIED_SCALE;
+        TechnologyTree.MapResearchCost = this.lMaps.get(this.iActiveMapID).mapData.ResearchCost;
     }
     @Shadow
     private void updateWorldMap() {
